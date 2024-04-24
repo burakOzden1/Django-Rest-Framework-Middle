5668479a06e47feabbb944fd9e47e62b6b78559e
Zeki Burak Özden <zburakozden@gmail.com>
2024-04-24 19:43:52 +0300

    concrete view ile birlikte yorum ekleme silme listeleme islemleri yapildi.

--- kitaplar/api/views.py
+++ kitaplar/api/views.py
@@ -2,9 +2,10 @@ from rest_framework.generics import GenericAPIView
 from rest_framework.mixins import ListModelMixin, CreateModelMixin
 
 from rest_framework import generics
+from rest_framework.generics import get_object_or_404
 
 from kitaplar.api.serializers import KitapSerializer, YorumSerializer
-from kitaplar.models import Kitap
+from kitaplar.models import Kitap, Yorum
 
 
 class KitapListCreateAPIView(generics.ListCreateAPIView):
@@ -15,6 +16,30 @@ class KitapListCreateAPIView(generics.ListCreateAPIView):
 class KitapDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Kitap.objects.all()
     serializer_class = KitapSerializer
+
+
+class YorumListAPIView(generics.ListCreateAPIView):
+    queryset = Yorum.objects.all()
+    serializer_class = YorumSerializer
+
+
+class YorumCreateAPIView(generics.CreateAPIView):
+    queryset = Yorum.objects.all()
+    serializer_class = YorumSerializer
+
+    def perform_create(self, serializer):
+        # path('kitaplar/<int:kitap_pk>/yorum_yap', api_views.YorumCreateAPIView.as_view(), name='yorum-yap')
+        kitap_pk = self.kwargs.get('kitap_pk')
+        kitap = get_object_or_404(Kitap, pk=kitap_pk)
+        serializer.save(kitap=kitap)
+
+
+class YorumDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Yorum.objects.all()
+    serializer_class = YorumSerializer
+
+
+
 
 
 
