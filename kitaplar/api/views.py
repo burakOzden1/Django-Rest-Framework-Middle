53ee94bbbcab55a4501f7e816851863d6ee0f615
Zeki Burak Özden <zburakozden@gmail.com>
2024-04-24 21:05:28 +0300

    permissions lar ile ilgili bazi islemler yapildi, django rest ekranina login ozelligi eklendi, permission_classes ozelligi ile api ye erisimler yetkilendirildi ve kendi permission_class imizi olusturarak ozellestirilmis yetkiler olusturduk.

--- kitaplar/api/views.py
+++ kitaplar/api/views.py
@@ -3,6 +3,8 @@ from rest_framework.mixins import ListModelMixin, CreateModelMixin
 
 from rest_framework import generics
 from rest_framework.generics import get_object_or_404
+from rest_framework import permissions
+from kitaplar.api.permissions import IsAdminUserOrReadOnly
 
 from kitaplar.api.serializers import KitapSerializer, YorumSerializer
 from kitaplar.models import Kitap, Yorum
@@ -11,21 +13,25 @@ from kitaplar.models import Kitap, Yorum
 class KitapListCreateAPIView(generics.ListCreateAPIView):
     queryset = Kitap.objects.all()
     serializer_class = KitapSerializer
+    permission_classes = [IsAdminUserOrReadOnly]
 
 
 class KitapDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Kitap.objects.all()
     serializer_class = KitapSerializer
+    permission_classes = [IsAdminUserOrReadOnly]
 
 
 class YorumListAPIView(generics.ListCreateAPIView):
     queryset = Yorum.objects.all()
     serializer_class = YorumSerializer
+    # permission_classes = [permissions.IsAdminUser]
 
 
 class YorumCreateAPIView(generics.CreateAPIView):
     queryset = Yorum.objects.all()
     serializer_class = YorumSerializer
+    # permission_classes = [permissions.IsAdminUser]
 
     def perform_create(self, serializer):
         # path('kitaplar/<int:kitap_pk>/yorum_yap', api_views.YorumCreateAPIView.as_view(), name='yorum-yap')
@@ -37,6 +43,7 @@ class YorumCreateAPIView(generics.CreateAPIView):
 class YorumDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Yorum.objects.all()
     serializer_class = YorumSerializer
+    # permission_classes = [permissions.IsAdminUser]
 
 
 
