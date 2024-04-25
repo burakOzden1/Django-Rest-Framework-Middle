2cae5cd95de9fd59887087a83cc8a2f648b6ff12
Zeki Burak Özden <zburakozden@gmail.com>
2024-04-25 13:08:33 +0300

    api uzerinden kitap kayitlarini cektik ve otomatik olarak serializers'imize kitaplar ekledik. Bu ders cok onemli!!!!!!!!!! -https://www.youtube.com/watch?v=q5Nv1m_OZr8&list=PLtf2C1UGjgPB1rg4lggEu0qET8jy9FduQ&index=10

--- scripts/fake_data.py
+++ scripts/fake_data.py
@@ -10,9 +10,13 @@ django.setup()
 from django.contrib.auth.models import User
 
 from faker import Faker
-def set_user():
-    fake = Faker(['en_US'])
+import requests
 
+from kitaplar.api.serializers import KitapSerializer
+
+fake = Faker(['en_US'])
+
+def set_user():
     f_name = fake.first_name()
     l_name = fake.last_name()
     u_name = f'{f_name.lower()}_{l_name.lower()}'
@@ -36,3 +40,40 @@ def set_user():
     user.set_password('testing321..')
     user.save()
     print('Kullanici kaydedildi', u_name)
+
+
+from pprint import pprint
+
+def kitap_ekle(konu):
+    # url = 'https://openlibrary.org/search.json?q=love'
+    url = 'https://openlibrary.org/search.json'
+    payload = {'q': konu}
+    response = requests.get(url, params=payload)
+
+    if response.status_code != 200:
+        print('Hatali istek yapildi:', response.status_code)
+        return
+    
+    jsn = response.json()
+    # pprint(jsn)
+
+    # print(response.url)
+
+    kitaplar = jsn.get('docs')
+
+    for kitap in kitaplar:
+        kitap_adi = kitap.get('title') 
+        data = dict(
+            isim = kitap_adi,
+            yazar = kitap.get('author_name')[0], # birden fazla yazar gelirse ilk yazari al dedik.
+            aciklama = '-'.join(kitap.get('author_name')),
+            yayin_tarihi = fake.date_time_between(start_date='-10y', end_date='now', tzinfo=None),
+        )
+        # pprint(data)
+
+        serializer = KitapSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            print('Kitap kaydedildi: ', kitap_adi)
+        else:
+            continue
